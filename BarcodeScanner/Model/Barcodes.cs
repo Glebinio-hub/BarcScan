d78746731855bee65cf01caa4e5cc93866d51fab
Glebinio <98263503+Glebinio-hub@users.noreply.github.com>
2025-02-19 01:19:00 +0300

    Реализовал структуру паттерна MVVM и модель данных

---
+++ BarcodeScanner/Model/Barcodes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanner.Model
+{
+    public class Barcodes
+    {
+		private string barcode;
+
+		public string Barcode
+		{
+			get { return barcode; }
+			set { barcode = value; }
+		}
+
+		private int articul;
+
+		public int Articul
+		{
+			get { return articul; }
+			set { articul = value; }
+		}
+
+		private string image;
+
+		public string Image
+		{
+			get { return image; }
+			set { image = value; }
+		}
+
+
+		private string description;
+
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
+		}
+
+
+	}
+}
